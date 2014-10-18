65d583bbe6ec1eb2547cda6b93df6e4723221b63
lpatmo <linda.peng@alumni.duke.edu>
2014-10-18 18:16:59 -0400

    more styles

--- app/forms.py
+++ app/forms.py
@@ -8,6 +8,6 @@ class ProposeNewHangout(Form):
 	hangout_date = DateField('hangout_date (mm/dd/yyyy)',validators = [DataRequired()], format='%m/%d/%Y')
 	level = SelectField('level', validators = [DataRequired()], choices = [('1', '1'), ('2', '2'), ('3', '3')])
 	language = SelectField('language', validators = [DataRequired()], choices = [('1', '1'), ('2', '2'), ('3','3'), (
-		'4', '4'), ('5', '5')])
+		'4', '4'), ('5', '5'), ('6', '6')])
 	description = TextField('description', validators = [DataRequired()])
 	status = IntegerField('Status')
