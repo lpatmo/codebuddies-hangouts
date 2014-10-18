c4b429ed4aece75a06aa8eeab9cdc7a11dfdd7ee
Muhammed Ziauddin <muhammedz786@gmail.com>
2014-10-18 18:54:32 -0400

    changed values to show javascript instead of 1

--- app/forms.py
+++ app/forms.py
@@ -6,8 +6,8 @@ class ProposeNewHangout(Form):
 	hangout_id = IntegerField('Status')
 	title = TextField('title', validators=[DataRequired()])
 	hangout_date = DateField('hangout_date (mm/dd/yyyy)',validators = [DataRequired()], format='%m/%d/%Y')
-	level = SelectField('level', validators = [DataRequired()], choices = [('1', 'Beginner'), ('2', 'Intermediate'), ('3', 'Advanced')])
-	language = SelectField('language', validators = [DataRequired()], choices = [('1', 'Javascript'), ('2', 'HTML/CSS'), ('3','Python'), (
-		'4', 'Ruby-on-Rails'), ('5', 'Git'), ('6', 'Data Structures and Algorithms')])
+	level = SelectField('level', validators = [DataRequired()], choices = [('Beginner', 'Beginner'), ('Intermediate', 'Intermediate'), ('Advanced', 'Advanced')])
+	language = SelectField('language', validators = [DataRequired()], choices = [('Javascript', 'Javascript'), ('HTML/CSS', 'HTML/CSS'), ('Python','Python'), (
+		'Ruby-on-Rails', 'Ruby-on-Rails'), ('Git', 'Git'), ('Data Structures and Algorithms', 'Data Structures and Algorithms')])
 	description = TextField('description', validators = [DataRequired()])
 	status = IntegerField('Status')
