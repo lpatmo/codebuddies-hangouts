870a766fd9d4a558526a6f1f65482f57c5bfb4a2
lpatmo <linda.peng@alumni.duke.edu>
2014-10-18 15:39:27 -0400

    dropped table and rebuilt table

--- app/db_create.py
+++ app/db_create.py
@@ -5,6 +5,8 @@ from config import DATABASE_PATH
 with sqlite3.connect(DATABASE_PATH) as connection:
 	c = connection.cursor()
 
+	# c.execute("""DROP TABLE hangout""")
+
 	c.execute("""CREATE TABLE hangout( hangout_id INTEGER PRIMARY KEY
 		AUTOINCREMENT,
 		title TEXT NOT NULL, level TEXT NOT NULL, language TEXT NOT NULL, hangout_date DATE NOT NULL, description TEXT NOT NULL, status INTEGER NOT NULL)""")
