bc952259aea02da51ed992c397b4eb3bd869e9ad
Muhammed Ziauddin <muhammedz786@gmail.com>
2014-10-18 18:49:52 -0400

    changed choiceces

--- app/forms.py
+++ app/forms.py
@@ -6,8 +6,8 @@ class ProposeNewHangout(Form):
 	hangout_id = IntegerField('Status')
 	title = TextField('title', validators=[DataRequired()])
 	hangout_date = DateField('hangout_date (mm/dd/yyyy)',validators = [DataRequired()], format='%m/%d/%Y')
-	level = SelectField('level', validators = [DataRequired()], choices = [('1', '1'), ('2', '2'), ('3', '3')])
-	language = SelectField('language', validators = [DataRequired()], choices = [('1', '1'), ('2', '2'), ('3','3'), (
-		'4', '4'), ('5', '5'), ('6', '6')])
+	level = SelectField('level', validators = [DataRequired()], choices = [('1', 'Beginner'), ('2', 'Intermediate'), ('3', 'Advanced')])
+	language = SelectField('language', validators = [DataRequired()], choices = [('1', 'Javascript'), ('2', 'HTML/CSS'), ('3','Python'), (
+		'4', 'Ruby-on-Rails'), ('5', 'Git'), ('6', 'Data Structures and Algorithms')])
 	description = TextField('description', validators = [DataRequired()])
 	status = IntegerField('Status')
