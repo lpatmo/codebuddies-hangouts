bc59c4725bd53eb4268139eabfee20dc9b14c08b
Muhammed Ziauddin <muhammedz786@gmail.com>
2014-10-18 13:47:27 -0400

    flask-files

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+#defines the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
